3b8de1c64b6fbb1353d34ec9d850d13793f3872d
tobby48 <tobby48@DESKTOP-LCJ5IV8>
2023-08-26 19:44:01 +0900

    내가~~~코드를 작성하였습니다.

---
+++ aa/src/aa/Main.java
@@ -0,0 +1,24 @@
+package aa; 
+
+
+/**
+ * <pre>
+ * aa 
+ * Main.java
+ *
+ * 설명 :
+ * </pre>
+ * 
+ * @since : 2023. 7. 22.
+ * @author : tobby48
+ * @version : v1.0
+ */
+public class Main {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
